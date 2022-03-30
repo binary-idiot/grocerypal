72443fc1fbf03afda3663fc43bb102b30c4a361d
Jonah Middleton <jonah.middleton@gmail.com>
2022-03-30 01:29:31 -0400

    Feat: Implement service worker via vite-plugin-pwa

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,39 @@ import { fileURLToPath, URL } from "url";
 
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), 
+    VitePWA({
+      includeAssets: ['smask-icon.svg', 'favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
+      manifest: {
+        name: 'Shopping Cart',
+        short_name: 'Shopping Cart',
+        description: 'Manage your shopping with a sharable lists and a cart calculator',
+        theme_color: '#2b5797',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+          }
+        ]
+      }
+    })
+  ],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
