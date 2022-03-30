74c8c1ab39833c1a5392f301051459bbbd1d5d38
Jonah Middleton <jonah.middleton@gmail.com>
2022-03-30 12:25:44 -0400

    Feat: Add prompt to update app

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,13 @@
 <template>
   <RouterView />
+  <ReloadPrompt />
 </template>
 
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router';
-import { useRegisterSW } from 'virtual:pwa-register/vue';
-
-useRegisterSW();
+  import { RouterLink, RouterView } from 'vue-router';
+  import ReloadPrompt from './components/system/ReloadPrompt.vue';
 </script>
 
-<style module lang="scss">
-@import "@/assets/base.scss";
-
+<style lang="scss">
+  @import "@/assets/base.scss";
 </style>
---
+++ src/components/system/ReloadPrompt.vue
@@ -0,0 +1,33 @@
+<template>
+	<div class="toast" v-if="offlineReady || needRefresh">
+		<div class="message">
+			<span v-if="offlineReady">
+				Ready to work offline
+			</span>
+			<span v-else>
+				Updates available
+			</span>
+		</div>
+		<button v-if="needRefresh" @click="updateServiceWorker()">
+			Update
+		</button>
+		<button @click="close">
+			Close
+		</button>
+	</div>
+</template>
+
+<script setup lang="ts">
+	import { useRegisterSW } from 'virtual:pwa-register/vue';
+
+	const { offlineReady, needRefresh, updateServiceWorker } = useRegisterSW();
+
+	const close = async() => {
+		offlineReady.value = false;
+		needRefresh.value = false;
+	}
+</script>
+
+<style module lang="scss">
+
+</style>
