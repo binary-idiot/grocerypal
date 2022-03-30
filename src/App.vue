0dc4104704c76324a72f038afa0bcd884f22123c
Jonah Middleton <jonah.middleton@gmail.com>
2022-03-30 03:17:15 -0400

    Fix: Add service worker registration code

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,10 @@
 </template>
 
 <script setup lang="ts">
-import { RouterLink, RouterView } from "vue-router";
+import { RouterLink, RouterView } from 'vue-router';
+import { useRegisterSW } from 'virtual:pwa-register/vue';
+
+useRegisterSW();
 </script>
 
 <style module lang="scss">
